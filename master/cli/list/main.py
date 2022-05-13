247912692209aab3754877186a8d1f64ddb67c32
Bo Cimino <ciminobo@protonmail.com>
2022-05-12 20:42:12 -0500

    change: MASTER_10: Omit closed tasks by default
    This changes the list subcommand to omit closed tasks by default.

--- master/cli/list/main.py
+++ master/cli/list/main.py
@@ -4,5 +4,15 @@ from master.Project import Project
 def do_list(args):
     p = Project.loadFromDisk(args.project)
 
+    if args.filter:
+        args.filter += ' and'
+    if 'closed' not in args.filter:
+        args.filter += ' t.stage != "closed"'
+    else:
+        args.filter += ' True'
+
+    s = []
     for p, root, tasks in p.filteredTasks(args.filter, args.project):
-        print('\n'.join([f'{root}/{t.title}' for t in tasks]))
+        s.extend([f'{root}/{t.title}' for t in tasks])
+
+    print('\n'.join(s).strip())

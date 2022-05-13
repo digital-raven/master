8a6f0b66bf76b5cdecdf6f92e7c6a780cda8232f
Bo Cimino <ciminobo@protonmail.com>
2022-05-13 01:38:12 -0500

    fix: MASTER_3: Fix bug in editing
    master edit will now save the edited changes to the correct place.

--- master/cli/edit/main.py
+++ master/cli/edit/main.py
@@ -2,15 +2,17 @@ from master.Project import Project
 
 
 def do_edit(args):
-    p = Project.loadFromDisk('./')
+    if not args.task.startswith('./'):
+        args.task = './' + args.task
+
+    proj_path = '/'.join(args.task.split('/')[:-1])
+    p = Project.loadFromDisk(proj_path)
 
     if args.task.endswith('.rst'):
         args.task = args.task[:-4]
 
-    if args.task.startswith('./'):
-        args.task = args.task[2:]
-
-    args.task = ''.join(args.task.replace('/', '.'))
+    if '/' in args.task:
+        args.task = args.task.split('/')[-1]
 
     task = eval(f'p.{args.task}')
     p.updateTask(task, editor='vim')
